2f841db2e7f6d820849fba2cf88a128c457a5def
s18643-pja <61684678+s18643-pja@users.noreply.github.com>
2020-04-27 10:56:03 +0200

    Initial commit

---
+++ Assets/Scripts/Canonball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canonball : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed = 5;
+    private Rigidbody2D rb;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = transform.right * -1 * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("MyPlayer"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
